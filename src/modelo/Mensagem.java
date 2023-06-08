44eee423c8a1c0dd2d731d33d6f0d3f8bfb8423f
MatheusSousa5068 <matheus5068trab@gmail.com>
2023-06-07 22:48:45 -0300

    criando classe mensagem (#3)
    Co-authored-by: MatheusSousa5068 <matheusoficial.sousa@gmail.com>

--- src/modelo/Mensagem.java
+++ src/modelo/Mensagem.java
@@ -1,4 +1,44 @@
 package modelo;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Mensagem {
+    private int id;
+    private String texto;
+    private Individual emitente;
+    private Participante destinatario;
+    private LocalDateTime datahora;
+
+    public Mensagem(String texto, Individual emitente, Participante destinatario) {
+        this.texto = texto;
+        this.emitente = emitente;
+        this.destinatario = destinatario;
+
+        this.datahora = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Individual getEmitente() {
+        return emitente;
+    }
+
+    public Participante getDestinatario() {
+        return destinatario;
+    }
+
+    public LocalDateTime getDatahora() {
+        return datahora;
+    }
 }
