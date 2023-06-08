c62cfa9fd9ae45135c7d3db3bb072d1bf589701e
MatheusSousa5068 <matheusoficial.sousa@gmail.com>
2023-06-07 23:07:36 -0300

    criando classe individual

--- src/modelo/Individual.java
+++ src/modelo/Individual.java
@@ -1,4 +1,39 @@
 package modelo;
 
-public class Individual {
+import java.util.ArrayList;
+
+public class Individual extends Participante {
+    private String senha;
+    private boolean administrador;
+    private ArrayList<Mensagem> enviadas;
+    private ArrayList<Grupo> grupos;
+
+    public Individual(String senha, boolean administrador) {
+        this.senha = senha;
+        this.administrador = administrador;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public boolean isAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        this.administrador = administrador;
+    }
+
+    public ArrayList<Mensagem> getEnviadas() {
+        return enviadas;
+    }
+
+    public ArrayList<Grupo> getGrupos() {
+        return grupos;
+    }
 }
