a82042050597ac60306beeb81443e28279fda679
Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>
2023-06-07 22:28:05 -0300

    finalizando a classe grupo

--- src/modelo/Grupo.java
+++ src/modelo/Grupo.java
@@ -3,5 +3,20 @@ package modelo;
 import java.util.ArrayList;
 
 public class Grupo {
-    private ArrayList<Individual> individuos :
+    private ArrayList<Individual> individuos;
+
+    public Grupo(){
+        this.individuos = new ArrayList<>();
+    }
+
+    public ArrayList<Individual> getIndividuos() {
+        return individuos;
+    }
+
+    @Override
+    public String toString() {
+        return "Grupo{" +
+                "individuos=" + individuos +
+                '}';
+    }
 }
