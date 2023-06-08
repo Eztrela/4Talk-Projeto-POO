387bb4f57dedf18ab97f607bfbb1af7021ca2326
Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>
2023-06-07 23:10:04 -0300

    finalizando a classe grupo (#2)
    * finalizando a classe grupo
    
    * Fazendo com que grupo herde de participante como requisitado em review

--- src/modelo/Grupo.java
+++ src/modelo/Grupo.java
@@ -2,6 +2,21 @@ package modelo;
 
 import java.util.ArrayList;
 
-public class Grupo {
-    private ArrayList<Individual> individuos :
+public class Grupo extends Participante{
+    private ArrayList<Individual> individuos;
+
+    public Grupo(){
+        this.individuos = new ArrayList<>();
+    }
+
+    public ArrayList<Individual> getIndividuos() {
+        return individuos;
+    }
+
+    @Override
+    public String toString() {
+        return "Grupo{" +
+                "individuos=" + individuos +
+                '}';
+    }
 }
