85149aa4b5679039dc3b781cae4df2b594c05361
Pablo de Lima Marques Estrela <102769647+Eztrela@users.noreply.github.com>
2023-06-07 23:12:04 -0300

    finalizando classe participante (#4)

--- src/modelo/Participante.java
+++ src/modelo/Participante.java
@@ -1,4 +1,33 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Participante {
+    private String nome;
+    private ArrayList<Mensagem> recebidas;
+
+    public Participante(String nome){
+        this.nome = nome;
+        recebidas = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Mensagem> getRecebidas() {
+        return recebidas;
+    }
+
+    @Override
+    public String toString() {
+        return "Participante{" +
+                "nome='" + nome + '\'' +
+                ", recebidas=" + recebidas +
+                '}';
+    }
 }
